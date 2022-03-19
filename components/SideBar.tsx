45380af20412d57f93b8373b4b03df80ad323a5c
Olatayo Olayemi <olatayo.olayemi.peter@gmail.com>
2022-03-19 12:11:41 +0100

    wip

--- components/SideBar.tsx
+++ components/SideBar.tsx
@@ -6,9 +6,7 @@ const SideBar = () => {
         <div className="blog-sidebar">
             <div className="single-sidebar text-center">
                 <Advert placement="sidebar" />
-                <iframe scrolling='no' frameBorder='0' className='iframe-ads' width='300' height='250' src="https://refbanners.com/I?tag=d_1456057m_45031c_&site=1456057&ad=45031" ></iframe>
                 <Advert placement="sidebar" />
-                <iframe scrolling='no' frameBorder='0' className='iframe-ads' width='300' height='600' src="https://refbanners.com/I?tag=d_1456057m_38069c_&site=1456057&ad=38069" ></iframe>
         
               </div>
         </div>

57e70e0ef9f43f78db400566a0c6471307915df8
Olatayo Olayemi <olatayo.olayemi.peter@gmail.com>
2022-04-19 11:38:31 +0100

    wip

--- pages/fixtures/[id].tsx
+++ pages/fixtures/[id].tsx
@@ -11,101 +11,103 @@ import Advert from '../../components/Advert'
 import Image from 'next/image'
 
 type Props = {
-    fixture: IFixture
-    errors?: object
+  fixture: IFixture
+  errors?: object
 }
 
 const FixtureDetails = ({ fixture, errors }: Props) => {
-    const date = new Date(fixture.streaming_at).toLocaleDateString('default', {
-        day: '2-digit',
-        month: 'short',
-        year: 'numeric'
-    })
+  const date = new Date(fixture.streaming_at).toLocaleDateString('default', {
+    day: '2-digit',
+    month: 'short',
+    year: 'numeric',
+  })
 
-    const time = new Date(fixture.streaming_at).toLocaleTimeString('default', {
-        hour: 'numeric',
-        minute: 'numeric',
-        hour12: true
-    })
+  const time = new Date(fixture.streaming_at).toLocaleTimeString('default', {
+    hour: 'numeric',
+    minute: 'numeric',
+    hour12: true,
+  })
 
-    const pageTitle = `${fixture.competition} - ${fixture.home_team.name} vs ${fixture.away_team.name}`
+  const pageTitle = `${fixture.competition} - ${fixture.home_team.name} vs ${fixture.away_team.name}`
 
-    return (
-        <Layout title={pageTitle}>
-            <BreadCrumb title={pageTitle}>
-                <li>
-                    <Link href="/fixtures">
-                        <a>Fixtures</a>
-                    </Link>
-                </li>
-            </BreadCrumb>
-            <Advert placement="header" />
+  return (
+    <Layout title={pageTitle}>
+      <BreadCrumb title={pageTitle}>
+        <li>
+          <Link href='/fixtures'>
+            <a>Fixtures</a>
+          </Link>
+        </li>
+      </BreadCrumb>
+      <Advert placement='header' />
 
-            <BlogLayout>
-                <div className="part-text">
-                    <a className="title">{pageTitle}</a>
-                    <ul className="meta-info">
-                        <li>
-                            <span className="icon">
-                                <i className="far fa-calendar-alt"></i>
+      <BlogLayout>
+        <div className='part-text'>
+          <a className='title'>{pageTitle}</a>
+          <ul className='meta-info'>
+            <li>
+                            <span className='icon'>
+                                <i className='far fa-calendar-alt'></i>
                             </span>{' '}
-                            {date}
-                        </li>
-                        <li>
-                            <span className="icon">
-                                <i className="far fa-clock"></i>
+              {date}
+            </li>
+            <li>
+                            <span className='icon'>
+                                <i className='far fa-clock'></i>
                             </span>{' '}
-                            {time}
-                        </li>
-                    </ul>
+              {time}
+            </li>
+          </ul>
 
 
-                    <div className="container mx-auto text-center">
-                      
-                        <div className="embed-responsive embed-responsive-4by3">
-                            <iframe
-                                className="embed-responsive-item"
-                                src={fixture.stream_source}
-                                sandbox="allow-scripts allow-same-origin"
-                                id={`Fixture - ${fixture.id}`}
-                                width="600"
-                                height="490"
-                                frameBorder="0"
-                                allowFullScreen></iframe>
-                        </div>
-                        
-                      
-                    </div>
+          <div className='container mx-auto text-center'>
 
-                    <ShareButtons />
-                </div>
-            </BlogLayout>
-        </Layout>
-    )
+            <div className='embed-responsive embed-responsive-4by3'>
+              <iframe
+                className='embed-responsive-item'
+                src={fixture.stream_source}
+                sandbox='allow-scripts allow-same-origin'
+                id={`Fixture - ${fixture.id}`}
+                width='600'
+                height='490'
+                frameBorder='0'
+                allowFullScreen></iframe>
+            </div>
+            <div className='alert alert-info pt-2 pb-2'>
+              If lagging(slow) please change your browser to desktop mode, then double tap the play button to start the
+              stream
+            </div>
+          </div>
+
+          <ShareButtons />
+        </div>
+      </BlogLayout>
+    </Layout>
+  )
 }
 
 export const getServerSideProps: GetServerSideProps = async ({ params }) => {
-    try {
-        const id = params?.id
+  try {
+    const id = params?.id
 
-        const response = await http.get(`/api/fixtures/${id}`)
-        const fixture = response.data.data
+    const response = await http.get(`/api/fixtures/${id}`)
+    const fixture = response.data.data
 
-        if (!fixture) {
-            return {
-                notFound: true
-            }
-        }
-        return {
-            props: {
-                fixture
-            }
-        }
-    } catch (error) {
-        return {
-            props: { error }
-        }
+    if (!fixture) {
+      return {
+        notFound: true,
+      }
     }
+    return {
+      props: {
+        fixture,
+      },
+    }
+  } catch (error) {
+    return {
+      props: { error },
+    }
+  }
 }
 
 export default FixtureDetails
